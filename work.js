6ffb8e13ea7a688eb84b3bbfd84a4b0fb7cb18f0
Young don <justicekwabenaagyenim@gmail.com>
2024-09-18 15:44:00 +0000

    Add files via upload

---
+++ work.js
@@ -0,0 +1,28 @@
+let age = 22;
+if (age > 18) {
+    console.log("You are an adult");
+}
+
+let grade = 88;
+if (grade > 90) {
+    console.log("A");
+} else {
+    console.log("B");
+}
+
+ 
+let income = 55000;
+let age = 32
+
+if (income > 50000) {
+    if (age > 30) {
+        console.log("You are eligible for a loan");
+    }
+}
+
+
+let userInput = ""; 
+if (userInput === "") {
+    console.log("Please enter a value");
+}
+
